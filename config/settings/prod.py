2cdfde691d91b1ce91e18d57d603c5982bd1befa
Your Name <you@example.com>
2026-01-07 00:26:33 +0330

    setup database

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,11 +9,19 @@ DEBUG = False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
-# Database - SQLite for all environments
+# Database - MySQL for production
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME', default='skycore1_db'),
+        'USER': config('DB_USER', default='skycore1_user'),
+        'PASSWORD': config('DB_PASSWORD', default='Hamed1385'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
